0f829aaf744c6ae57296fbafd099edbfe177ced0
Nekketsu4 <101641229+Nekketsu4@users.noreply.github.com>
2023-11-03 06:37:12 +0300

    fix views.py

--- api_multimedia/views.py
+++ api_multimedia/views.py
@@ -1,15 +1,13 @@
 from rest_framework import generics
 from django.db.models.functions import Concat
-from django.db.models import F, Value, ExpressionWrapper, CharField, OuterRef, FilteredRelation, Q, Count, Func
+from django.db.models import F, Value, ExpressionWrapper, CharField
 
-from .models import Album, Artist, Track
+from .models import Album
 from .serializers import MultimediaSerializer
 
 
 class MultimediaListView(generics.ListAPIView):
     serializer_class = MultimediaSerializer
-
-    # tracks = Track.objects.filter(album__name=)
 
     def get_queryset(self):
 
@@ -20,4 +18,5 @@ class MultimediaListView(generics.ListAPIView):
             artist_name=Concat(F('artist__name'), Value('@'), F('name')))
         if sorting is not None:
             queryset = queryset.order_by(sorting)
+
         return queryset
